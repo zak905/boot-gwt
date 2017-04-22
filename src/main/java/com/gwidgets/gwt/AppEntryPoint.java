d0e4d1bef176d31a412dac277caace57134319a4
zak905 <zakaria.amine88@gmail.com>
2017-04-22 16:38:24 +0200

    initial

---
+++ src/main/java/com/gwidgets/gwt/AppEntryPoint.java
@@ -0,0 +1,15 @@
+package com.gwidgets.gwt;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class AppEntryPoint implements EntryPoint {
+	
+	
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("zakaria"));
+		
+	}
+
+}
---
+++ src/test/java/com/gwidgets/BootGwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gwidgets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootGwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
